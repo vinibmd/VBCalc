5ff7d7c032b3cb1be958dd3ee6441ee2722ed6ef
vinibmd <drbeleze@gmail.com>
2024-08-21 09:43:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,18 +33,21 @@ st.title("Bem vindo(a)! :smile:")
 
 st.header("É um prazer ter você por aqui!")
 
-"""
-É com muita alegria que apresento para vocês o **VB Calc.1.0**, um aplicação
+st.markdown("""É com muita alegria que apresento para vocês o **VB Calc.1.0**, um aplicação
 multiplataforma (desktop, iOS e Android), desenvolvida por mim ([Dr. Vinícius Beleze](https://instagram.com/drviniciusbeleze))!
 
 Fique a vontade para utilizar a plataforma na sua prática clínica diária!
 
-Se houver qualquer dúvida quanto a utilização da plataforma, você pode acessar
-a página de [Tutorial](pages/tutorial.py) para aprender sobre as mecânicas e 
-as terminologias do App.
+Os conteúdos podem ser acessados na **barra lateral esquerda**.""")
+st.markdown("Ao utilizar a plataforma, você **CONCORDA** e **ACEITA** os **Termos de Uso**.")
 
-Ao utilizar a plataforma, você **CONCORDA** e **ACEITA** os [Termos de Uso](pages/termos.py).
+st.markdown("Segue um **Tutorial em Vídeo** sobre como utilizar a plataforma:")
 
+st.video("https://www.youtube.com/watch?v=lKSZnZggcto&list=RDlKSZnZggcto&start_radio=1")
+     
+st.markdown("Se houver qualquer outra dúvida quanto a utilização da plataforma, você pode acessar a página de **Tutorial**.")
+
+st.markdown("""
 Agora, se você está gostando da plataforma, que tal ajudar a continuar e manter esse
 projeto sempre ativo e grauito? [Apoie o VB Calc.](https://biolivre.com.br/vbcardiologia)
 
@@ -52,7 +55,7 @@ Qualquer dúvida, sugestão, reclamação ou proposta de colaboração me manda 
 no drbeleze@gmail.com.
 
 Um ótimo dia!
-"""
+""")
 
 
 
