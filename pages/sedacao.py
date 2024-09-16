e31a44b8c571ac82e08e32a79ef2f3828b1c4205
vinibmd <drbeleze@gmail.com>
2024-09-16 12:20:02 -0300

    Otimização de código.

--- pages/sedacao.py
+++ pages/sedacao.py
@@ -1,32 +1,12 @@
 import streamlit as st
+from main import barra_lateral
 
 st.set_page_config(
     page_title="VB Calc - Analgesia e Sedação",
     page_icon=":pill:"
 )
 
-    ######################
-logo_ext_color = "imagens/logo_extended_color.png"
-logo_ext_bw = "imagens/logo_extended_bw.png"
-logo_only_color = "imagens/logo_only_color.png"
-logo_only_bw = "imagens/logo_only_bw.png"
-logo_vbcalc = "imagens/VBCalc_logo.png"
-logo_vbcalc2 = "imagens/VBCalc_logo_ext.png"
-
-# st.sidebar.image(logo_vbcalc, use_column_width=True)
-st.sidebar.image(logo_vbcalc2, use_column_width=True)
-st.sidebar.link_button("Apoie o VB Calc.", "https://biolivre.com.br/vbcardiologia", type="primary", use_container_width=True)
-with st.sidebar:
-    st.page_link("main.py", label="Home")
-    st.page_link("pages/sedacao.py", label="Analgesia e Sedação")
-    st.page_link("pages/bloqueadores.py", label="Bloq. Neuromuscular")
-    st.page_link("pages/dva.py", label="Drogas Vasoativas")
-    st.page_link("pages/ecg.py", label="ECG")
-    st.page_link("pages/nitratos.py", label="Nitratos")
-    st.page_link("pages/ref.py", label="Referências")
-    st.page_link("pages/tutorial.py", label="Tutorial")
-    st.page_link("pages/termos.py", label="Termos de Uso")
-    ###########################
+barra_lateral()
     
 st.sidebar.markdown("---")
 st.sidebar.markdown("Desenvolvido por **[Dr. Vinícius Beleze](https://instagram.com/drviníciusbeleze)**")
