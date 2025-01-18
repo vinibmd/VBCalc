11dca03a30d052c834a1488c4027091adc4514be
vinibmd <68612996+vinibmd@users.noreply.github.com>
2025-01-18 10:09:12 -0300

    Update diversos.py

--- pages/diversos.py
+++ pages/diversos.py
@@ -12,34 +12,52 @@ barra_lateral()
 st.sidebar.markdown("---")
 st.sidebar.markdown("Desenvolvido por **[Dr. Vinícius Beleze](https://instagram.com/drviníciusbeleze)**")
 
+# Título
 st.title("Calculadoras Diversas:")
 
-imc_tab, ckd_tab = st.tabs(['Calculadora IMC', 'Clearence de Creatinina'])
+# Aba de cálculo de IMC
+imc_tab, ckd = st.tabs(['Calculadora IMC', 'Clearence de Creatinina'])
 
 with imc_tab:
+    # Inputs de altura e peso
     st.subheader('Altura (cm):')
     altura = st.number_input(
-    "Altura", 
-    value=None, 
-    placeholder="Altura em cm...", 
-    min_value=100,
-    max_value=200, 
-    label_visibility='collapsed'
+        "Altura", 
+        value=0, 
+        placeholder="Altura em cm...", 
+        min_value=0, 
+        max_value=300, 
+        label_visibility='collapsed'
     )
     st.subheader('Peso (kg)')
     peso = st.number_input(
-    "Peso", 
-    value=None, 
-    placeholder="Peso em Kg..", 
-    max_value=500.0, 
-    label_visibility='collapsed',
-    step=0.1,
-    format="%0.1f"
+        "Peso", 
+        value=0.0, 
+        placeholder="Peso em Kg...", 
+        min_value=0.0, 
+        max_value=500.0, 
+        label_visibility='collapsed', 
+        step=0.1, 
+        format="%.1f"
     )
-    st.subheader('IMC:')
-    if altura and peso:
-        altura_metros = altura / 100
-        imc_valor = peso / (altura_metros ** 2)
-        st.write(f'{imc_valor:.2f}')
-    else:
-        st.write('Por favor, insira um valor válido.')
+    
+    # Botões
+    col1, col2 = st.columns(2)  # Criar duas colunas para posicionar os botões
+    calcular = col1.button("Calcular")
+    limpar = col2.button("Limpar")
+
+    # Lógica dos botões
+    if calcular:
+        if altura > 0 and peso > 0:
+            altura_metros = altura / 100  # Converter altura para metros
+            imc_valor = peso / (altura_metros ** 2)  # Calcular IMC
+            st.subheader('IMC:')
+            st.write(f'**{imc_valor:.2f}**')  # Mostrar IMC com 2 casas decimais
+        else:
+            st.warning("Por favor, insira valores válidos para altura e peso.")
+
+    if limpar:
+        # Resetar os valores de altura e peso
+        st.session_state["Altura"] = 0
+        st.session_state["Peso"] = 0.0
+        st.experimental_rerun()  # Recarrega a página para atualizar os inputs
